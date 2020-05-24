764afc07d027543d936b100aa80624dc931d517e
AlexandraMello <65859099+AlexandraMello@users.noreply.github.com>
2020-05-24 14:32:50 -0300

    Projeto COMUnica
    Em 2020, o mundo foi atingido de uma maneira inesperada gerando estabilidade em inúmeras áreas. O desperdício nunca foi tão grande quanto o número de pessoas sem conseguir comprara comida. Tendo em vista a situação atual da fome e perda não só de alimentos  criamos este projeto.

---
+++ receber.php
@@ -0,0 +1,39 @@
+<?php
+
+if (isset($_POST['temail'])) && !empaty($_POST['temail']){
+  // code...
+
+  $email = addslashes($_POST['temail']);
+  $nome = addslashes($_POST['tnome']);
+  $cpf = addslashes($_POST['tcpf']);
+  $endereço = addslashes($_POST['tend']);
+  $produto = addslashes($_POST['tprod']);
+  $validade = addslashes($_POST['tval']);
+  $quantidade = addslashes($_POST['tquant']);
+
+
+  $to = "contatoCOMUnic@br.com";
+  $subject = "Contato projeto COMUnica";
+  $body = "Nome:" .$nome. "\r\n".
+          "Email:" .$email. "\r\n".
+          "CPF" .$cpf. "\r\n".
+          "Endereço" .$endereço. "\r\n".
+          "Produto" .$produto. "\r\n".
+          "validade" .$validade. "\r\n".
+          "Quantidade" .$quantidade. "\r\n";
+
+  $header = "From = contatoCOMUNIc@br.com"."\r\n". "Replay-To:" .$email."\r\n". "X-Malier: PHP/"phpversion();
+  $error = new mysqli ($to, $subject, $body, $header)
+
+  if (email($to, $subject, $body, $header)) {
+      echo ("Cadastro enviado com sucesso para analise");
+  }else {
+      echo "Falha ao enviar cadastro (".$error->connect_errno.") ". $error->connect_errno;
+  }
+
+
+}
+
+
+
+ ?>
